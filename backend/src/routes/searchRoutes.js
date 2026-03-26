dac856262557e63b17fd0baa3fef604ff29c91d6
Sara Díaz <sdu1@alu.ua.es>
2026-03-26 12:40:22 +0100

    Eliminaciones en backend

--- backend/src/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { search } = require('../controllers/searchController')
-
-// GET /api/search?query=&universe=&personality=&mbtiType=
-router.get('/', search)
-
-module.exports = router
